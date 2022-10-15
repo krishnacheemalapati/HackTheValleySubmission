557e6c3d51413f85b8897618d19bac91dc75c2d9
krishnacheemalapati <krishkrishct@gmail.com>
2022-10-15 11:28:01 -0400

    Adding animations

---
+++ App.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+  var id = null;
+  const car = document.getElementById("car");
+  const btn = document.getElementById("journeybtn");
+  console.log("iwehofr");
+  alert("ioer");
+  btn.onclick = moveCar();
+  function moveCar() {
+    console.log("ayid");
+    alert("ioer");
+    var pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 10);
+    function frame() {
+      if (pos == 9950) {
+        clearInterval(id);
+      } else {
+        pos++;
+        car.style.left = pos + "px";
+      }
+    }
+  }
+});
